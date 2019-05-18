257c0e7c7ddb0f2497999195ddec04c7a66b5147
QWYNG <ikusawasi@gmail.com>
2019-05-18 23:23:57 +0900

    クラスを別ファイルに分ける searching nowを追加

--- echoes.rb
+++ echoes.rb
@@ -1,45 +1,13 @@
-# frozen_string_literal: true
-
-require 'rest-client'
 require 'dotenv/load'
-require 'base64'
-require 'json'
-require 'base64'
 require "awesome_print"
-
-class SpotifyClient
-  attr_reader :access_token
-
-  def initialize(client_id, client_secret)
-    base64id = Base64.strict_encode64("#{client_id}:#{client_secret}")
-
-    r = RestClient.post('https://accounts.spotify.com/api/token',
-                        { grant_type: :client_credentials },
-                        Authorization: "Basic #{base64id}")
-
-    @access_token = JSON.parse(r.body, symbolize_names: true)[:access_token]
-  end
-
-  def search_playlist(search_word)
-    response = RestClient.get('https://api.spotify.com/v1/search',
-                              Authorization: "Bearer #{@access_token}",
-                              params: { q: search_word, type: :playlist, limit: 50 })
-
-    JSON.parse(response.body, symbolize_names: true)[:playlists][:items]
-  end
-
-  def get_tracks_from_playlist(playlist)
-    response = RestClient.get(playlist[:href], Authorization: "Bearer #{@access_token}")
-
-    JSON.parse(response.body, symbolize_names: true)[:tracks]
-  end
-end
+require './spotify_client'
 
 spotify_client = SpotifyClient.new(ENV['ClIENT_ID'], ENV['ClIENT_SECRET'])
 
 puts 'Input Genre or Mood!'
 search_word = gets.chomp
 
+puts 'searching now...'
 playlists = spotify_client.search_playlist(search_word)
 
 result = {}
@@ -59,4 +27,5 @@ playlists.each do |playlist|
     end
   end
 end
+
 ap result, options = { color: { hash: :green } }
